eb0f64f0a78522e676cc5e5ea2eeed813ed32db3
Horus Sinhue Camacho <62359674+horuscamacho@users.noreply.github.com>
2023-05-30 19:23:04 -0600

    LISTO

--- src/App.js
+++ src/App.js
@@ -22,18 +22,28 @@ function App() {
       >
         <p>Username:</p>
         <input
+            //LE DIMOS UN NAME A LOS INPUTS PARA QUE SE PUEDAN ASOCIAR AL ESTADO DEL HOOK, TIENE QUE SER EL MISMO NOMBRE QUE EL KEY DEL ESTADO DEL HOOK DONDE LO INICIALIZAMOS
             name='username'
             onChange={(e) => handleChange(e)}
+
+            //Aquí se le pasa el valor del input // que está asociado al estado del hook por medio del name del input
+
             value={values.username}
         />
         <p>Password:</p>
         <input
+
             name='password'
             type='password'
             onChange={(e) => handleChange(e)}
+
+            //Aquí se le pasa el valor del input // que está asociado al estado del hook por medio del name del input
+
             value={values.password}
         />
-        {user ?
+
+        {//AQUÍ JUGAMOS CON EL TEXTO DEL BOTÓN DEPENDIENDO DEL ESTADO DE LA AUTENTICACIÓN (INIT, LOADING, SUCCESS, FAILED) DEL HOOK DE AUTENTICACIÓN
+          user ?
             <button type='button' onClick={() => logOut()}>Log Out</button> :
             <button type='button' onClick={() => {
               login(values.username, values.password)
@@ -41,7 +51,7 @@ function App() {
             }}>Login</button>}
 
         {/*AQUÍ JUGAMOS CON EL COMPORTAMIENTO DE LA UI DEPENDIENDO DEL ESTADO DE LA AUTENTICACIÓN (INIT, LOADING, SUCCESS, FAILED) DEL HOOK DE AUTENTICACIÓN*/}
-        {status === 'loading' && <p>Loading...</p>}
+        {status === 'loading' && <h1>Loading...</h1>}
         {status === 'failed' && <h1>Username or password incorrect</h1>}
         {status === 'success' && <h1>Welcome {user?.name}</h1>}
       </div>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -6,15 +6,19 @@
 
 import React from 'react';
 export function useForm(initialValues) {
+    // SETEAMOS EL ESTADO DE LOS INPUTS CON EL HOOK USESTATE /POR DEFECTO VA A TRAER UN OBJETO CON LOS VALORES INICIALES DE LOS INPUTS PARA PODER SETEARLOS Y ASOCIARLOS A LOS INPUTS
     const [values, setValues] = React.useState(initialValues);
 
+    // HANDLECHANGE ES UN MÉTODO QUE ACTUALIZA EL ESTADO DE LOS INPUTS
     function handleChange(e) {
         setValues({
             ...values,
             [e.target.name]: e.target.value
         })
     }
+    // RESET ES UN MÉTODO QUE RESETEA EL ESTADO DE LOS INPUTS PARA BORRAR LOS VALORES DE LOS INPUTS UNA VEZ QUE SE HAYA ENVIADO EL FORMULARIO
     function reset() {
+        // ESTE SETTIMEOUT ES PARA SIMULAR UNA PETICIÓN ASÍNCRONA QUE TARDARÍA 2 SEGUNDOS EN EJECUTARSE PARA EMPATARLO CON EL TIEMPO QUE TARDA LA SIMULACIÓN QUE HICE EN EL HOOK DE AUTENTICACIÓN
         setTimeout(() => {
             setValues(initialValues);
         }, 2000)
